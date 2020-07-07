51cf47b0078abec7067cae602b753eba12a4765a
brenolapreza <brenolapreza@gmail.com>
2020-07-06 21:18:27 -0300

    um pouco sobre heranca

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-//foi criado uma classe com o método constructor para pode instaciar
-class TodoList{
+//herança
+class List{
     constructor(){
-        //crio uma variável que recebe uma array vazia.
-        this.todos = [];
+        this.data = [];
     }
-    //criou um método (bem parecido com function)
-    addTodo(){
-        //esse método recebeu o .push para add uma array nova
-        this.todos.push('Novo Todo');
-        console.log(this.todos)
+
+    add(data){
+        this.data.push(data)
+        console.log(this.data)
     }
 }
-// foi instaciado o TodoList
+
+//extendendo List (herdando)
+class TodoList extends List{
+
+}
+
 const minhaLista = new TodoList();
 
-//toda vez que clica no botão, cria uma nova instância e repete todos os métodos estabelecidos
 document.getElementById('novotodo').onclick = function(){
-    //uma função que retorna o addTodo()
-    minhaLista.addTodo();
+    minhaLista.add('Novo Todo');
 }
